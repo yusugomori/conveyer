d13b44a16b0cb3e8ec0bcfe525dda3aa0402bd9e
Yusuke Sugomori <me@yusugomori.com>
2018-08-30 00:51:06 +0900

    release v0.0.3

--- conveyer/__init__.py
+++ conveyer/__init__.py
@@ -1,4 +1,4 @@
 from .readers import *
 from .package import convey, produce
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='conveyer',
-    version='0.0.2',
+    version='0.0.3',
     description='Automated machine learning library',
     author='Yusuke Sugomori',
     author_email='me@yusugomori.com',
