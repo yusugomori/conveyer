65ecf694ec50632ad1bb9027b5bff9ee5588939c
Yusuke Sugomori <yusuke_sugomori@micin.jp>
2018-08-30 12:54:44 +0900

    fix for multiclass

--- conveyer/metrics/precision.py
+++ conveyer/metrics/precision.py
@@ -2,10 +2,9 @@ import numpy as np
 from sklearn.metrics import precision_score
 
 
-def precision(preds, target, thres=0.5):
-    if len(preds.shape) == 1:
-        return precision_score(target, preds > thres)
+def precision(preds, target):
+    if len(np.unique(target)) == 2:
+        return precision_score(target, preds)
     else:
-        return precision_score(np.argmax(target, 1).astype('int32'),
-                               np.argmax(preds, 1),
+        return precision_score(target, preds,
                                average='macro')
